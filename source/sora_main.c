96d2ef462b21697ef7ba7cefaa33689f3640e09f
Yuu Sora <shenjiaqihi@gmail.com>
2019-05-26 16:10:37 +0800

    main: update

--- source/sora_main.c
+++ source/sora_main.c
@@ -15,7 +15,44 @@
 
 #include "include.h"
 
+/* ENC配置 */
 int16_t	ENC_Speed = 0;
+int32_t ENC_Distance = 0;
+#define ENC_PIT			PIT3
+#define ENC_Cycle		50
+
+/* FlexPWM配置 */
+#define Movement_SM		PWM_SM2
+#define Movement_Freq	200
+#define ESC_CH			PWM0_SM2_CHA
+#define STEER_CH		PWM0_SM2_CHB
+
+/* UART配置 */
+#define PC_UART			UART_0
+#define PC_Baud			256000//921600
+#define PC_RX_Size		64
+uint8_t PC_RX[PC_RX_Size] = { 0 };
+bool	PC_RX_Flag = false;
+uint8_t PC_RX_Counter = 0;
+#define PC_RX_DMA		DMA_CH5
+#define PC_TX_Size		64
+uint8_t PC_TX[PC_TX_Size] = { 0 };
+bool	PC_TX_Flag = false;
+#define PC_TX_DMA		DMA_CH6
+
+/* 通信配置 */
+typedef enum MSG_TAG
+{
+	HEARTBEAT,
+	UPDATE_WHEEL_ENCODER,
+	MCU_PANIC,
+	UPDATE_TWIST,
+	STOP_IMMEDIATELY
+}MSG_TAG;
+#define MSG_Cycle		50
+
+/* 函数声明 */
+uint8_t UART_Create_MSG(uint8_t buff[], MSG_TAG tag, void* value, uint8_t size);
 
 int main(void)
 {
@@ -27,26 +64,31 @@ int main(void)
 	/* LCD 初始化完成 */
 
 	/* PWM初始化 */
-	FlexPWM_Independent_Submodule_Init(PWM0, PWM_SM2, PWM_Signed_EdgeAligned, 200);	//舵机和电调均采用200Hz信号
-	FlexPWM_Independent_Channel_Init(PWM0_SM2_CHA);		//电调初始化
-	FlexPWM_Independent_Channel_Duty(PWM0_SM2_CHA, 10);	//电调初始占空比
-	FlexPWM_Independent_Channel_Init(PWM0_SM2_CHB);		//舵机初始化
-	FlexPWM_Independent_Channel_Duty(PWM0_SM2_CHB, 90);	//舵机初始占空比
+	FlexPWM_Independent_Submodule_Init(PWM0, Movement_SM, PWM_Signed_EdgeAligned, Movement_Freq);	//舵机和电调均采用200Hz信号
+	FlexPWM_Independent_Channel_Init(ESC_CH);		//电调初始化
+	FlexPWM_Independent_Channel_Duty(ESC_CH, 0);	//电调初始占空比
+	FlexPWM_Independent_Channel_Init(STEER_CH);		//舵机初始化
+	FlexPWM_Independent_Channel_Duty(STEER_CH, 0);	//舵机初始占空比
 	/*
 		此处应为遥控器输入信号检测初始化
 	*/
-	FlexPWM_Independent_Submodule_Init(PWM1, PWM_SM0, PWM_Unsigned_EdgeAligned, 700000);	 //WS2812所需800kHz通讯信号，降频以保证通讯稳定
-	FlexPWM_Independent_Channel_Init(PWM1_SM0_CHA);		//WS2812初始化
-	FlexPWM_Independent_Channel_Duty(PWM1_SM0_CHA, 0);	//WS2812初始占空比
+	//FlexPWM_Independent_Submodule_Init(PWM1, PWM_SM0, PWM_Unsigned_EdgeAligned, 700000);	 //WS2812所需800kHz通讯信号，降频以保证通讯稳定
+	//FlexPWM_Independent_Channel_Init(PWM1_SM0_CHA);		//WS2812初始化
+	//FlexPWM_Independent_Channel_Duty(PWM1_SM0_CHA, 0);	//WS2812初始占空比
 	/* PWM初始化完成 */
 
 	/* ENC采集初始化 */
-	ENC_Init_Period(PIT3, 50);
+	ENC_Init_Period(ENC_PIT, ENC_Cycle);
 	/* ENC采集初始化完成 */
 
 	/* UART初始化 */
-	UART_Com_Init(UART_0, 115200);	//连接车载PC
-	UART_Com_Init(UART_1, 115200);	//板载调试口
+	UART_Com_Init(PC_UART, PC_Baud);	//连接车载PC
+	UART_IRQ_EN(PC_UART, UART_RX_DMA);	
+	UART_IRQ_EN(PC_UART, UART_IDLE);
+	EDMA_UART_RX_Init(PC_RX_DMA, PC_UART);
+	EDMA_UART_TX_Init(PC_TX_DMA, PC_UART);
+	EDMA_UART_RX_Start(PC_RX_DMA, (uint32)PC_RX, PC_RX_Size);
+	//UART_Com_Init(UART_1, 115200);	//板载调试口
 	/* UART初始化完成 */
 
 	/* IIC初始化 */
@@ -62,7 +104,7 @@ int main(void)
 	/* SPI初始化完成 */
 
 	/* PIT中断初始化 */
-	PIT_IRQ_Init(PIT0, 1); //1ms一次中断
+	PIT_IRQ_Init(PIT0, MSG_Cycle);
 	/* PIT中断初始化完成 */
 
 	/* DMA初始化 */
@@ -70,22 +112,71 @@ int main(void)
 		此处应为DMA初始化
 	*/
 	/* DMA初始化完成 */
-
+	FlexPWM_Independent_Channel_Duty(STEER_CH, FlexPWM_PPMCal_us(Movement_Freq, 1500));
 	while (1U)
 	{
+		ENC_Speed = ENC_Get_Speed();
+		ENC_Distance = ENC_Get_Position();
+
+		/* LCD Display*/
 		LCD_P6x8Str(0, 1, "Main");
 		sprintf(text, "ENC: %05d", ENC_Speed);
 		LCD_P6x8Str(0, 2, text);
+		sprintf(text, "ENC: %010d", ENC_Distance);
+		LCD_P6x8Str(0, 3, text);
+
+		/* MSG 处理 */
+		if (PC_RX_Flag)
+		{
+			PC_RX_Flag = false;
+			EDMA_UART_RX_Start(PC_RX_DMA, (uint32)PC_RX, PC_RX_Size);
+		}
 	}
 }
 
 void PIT0_IRQHandler()
 {
+	uint8_t length = 0;
 	PIT_Flag_Clear(PIT0);
+	length = UART_Create_MSG(PC_TX, UPDATE_WHEEL_ENCODER, &ENC_Distance, sizeof(ENC_Distance));
+	UART_IRQ_EN(PC_UART, UART_TX_DMA);
+	EDMA_UART_TX_Start(PC_TX_DMA, (uint32)PC_TX, length);
 }
 
-void PIT3_IRQHandler()
+void UART0_RX_TX_IRQHandler(void)
 {
-	PIT_Flag_Clear(PIT3);
-	ENC_Speed = ENC_Get_Speed();
+	uint8 buff;
+	if (UART0->S1 & UART_S1_IDLE_MASK)
+	{
+		buff = UART0->D;
+		PC_RX_Counter = EDMA_UART_RX_Stop(PC_RX_DMA, (uint32)PC_RX);
+		UART0->CFIFO |= UART_CFIFO_RXFLUSH(1);
+		PC_RX_Flag = true;
+	}
+}
+
+void DMA6_DMA22_IRQHandler(void)
+{
+	if (DMA0->INT & (1 << 6))
+	{
+		DMA0->CINT |= DMA_CINT_CINT(6);
+		UART_IRQ_DIS(PC_UART, UART_TX_DMA);
+	}
+}
+
+uint8_t UART_Create_MSG(uint8_t buff[], MSG_TAG tag, void* value, uint8_t size)
+{
+	uint8_t i = 0;
+	uint8_t check = 0;
+	buff[0] = 0x55;
+	buff[1] = 0xAA;
+	buff[2] = tag;
+	buff[3] = size;
+	memcpy(buff + 4, value, size);
+	for (i = 0; i < size; i++)
+	{
+		check += buff[4 + i];
+	}
+	buff[4 + size] = check;
+	return 5 + size;
 }
